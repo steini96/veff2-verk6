3b06c69404328068b3856580e6751226a00da4dd
steini96 <steini.tki@gmail.com>
2019-04-05 19:12:37 +0000

    reyna að laga fyrir heroku

--- components/todo-item/TodoItem.js
+++ components/todo-item/TodoItem.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
-import css from './todoItem.css';
+import './todoItem.css';
 
 // Verkefni í lista á forsíðu
 export default function todoItem(props) {
@@ -9,10 +9,10 @@ const { todo } = props;
 
   return (
     <React.Fragment>
-      <div className={css.item}>
-        <input type="checkbox" className={css.item__input}/>
+      <div className="item">
+        <input type="checkbox" className="item__input"/>
         <p><Link as={"/todo/"+todo.id} href="/todo/?id=2"><p>{todo.title}</p></Link></p>
-        <p className = {css.item__due}>{todo.due}</p>
+        <p className = "css.item__due">{todo.due}</p>
       </div>
     </React.Fragment>
 
